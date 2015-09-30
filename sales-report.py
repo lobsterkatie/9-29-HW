73f29ff9242faf3de4673f7966c9ce8e4fbb45ca
Katie Byers <lobsterkatie@gmail.com>
2015-09-29 22:01:22 -0700

    switched to using dictionaries; commented the hell out of everything

--- sales-report.py
+++ sales-report.py
@@ -1,25 +1,56 @@
 """
 sales_report.py - Generates sales report showing the total number
                   of melons each sales person sold.
+
+Excessively commented because that's the point!
 """
 
-salespeople = []
-melons_sold = []
+#create a blank dictionary to hold sales data, keyed by salesperson
+sales_data = {}
+# salespeople = []
+# melons_sold = []
 
-f = open("sales_report.txt")
-for line in f:
+#open the sales report file for reading
+sales_report_file = open("sales-report.txt")
+
+#iterate through the file entry by entry (line by line), updating overall
+#sales data (sales_data dictionary) with that line's data
+for line in sales_report_file:
+
+    #pull whitespace off the end of the line
     line = line.rstrip()
+
+    #split the line at each pipe, put the entries into the entries list
     entries = line.split("|")
+
+    #get the name of the salesperson, the total sale, and the number of
+    #melons sold
     salesperson = entries[0]
+    total_sale = float(entries[1])
     melons = int(entries[2])
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
+    #search for salesperson in sales_data dictionary
+    #if found, update total sales and melon total
+    if salesperson in sales_data:
+        sales_data[salesperson]["total sales"] += total_sale
+        sales_data[salesperson]["total melons sold"] += melons
+
+    #if salesperson isn't in sales_data dictionary, add a new entry with that
+    #person's info
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+        sales_data[salesperson] = {"total sales": total_sale,
+                                   "total melons sold": melons}
 
 
-for i in range(len(salespeople)):
-    print "{} sold {} melons".format(salespeople[i], melons_sold[i])
+#QUESTION: DIFFERENCE BETWEEN USING KEY TO REFER TO JUST THE KEY AND USING KEY
+#TO REFER TO ITS VALUE (FEELS LIKE I'M DOING A LOT OF DEREFERENCING)
+
+#go through sales data and print info for each sales person
+for salesperson in sales_data:
+    #get data for current salesperson
+    melons = sales_data[salesperson]["total melons sold"]
+    sales = sales_data[salesperson]["total sales"]
+
+    #create and print string describing current salesperson's sales
+    outstr = "{name} sold {num_melons} melons for a total of ${gross:,.2f}"
+    print outstr.format(name=salesperson, num_melons=melons, gross=sales)
